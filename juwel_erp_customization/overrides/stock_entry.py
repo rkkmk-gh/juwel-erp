3883032d5855f00c24f3eee6b856bf2554f2d9a8
rkkmk-gh <60239664+rkkmk-gh@users.noreply.github.com>
2023-02-27 10:57:29 -0500

    Add files via upload

---
+++ juwel_erp_customization/overrides/stock_entry.py
@@ -0,0 +1,20 @@
+import frappe
+from frappe import _
+from erpnext.stock.doctype.stock_entry.stock_entry import StockEntry
+
+
+class JGStockEntry(StockEntry):
+
+    def get_gl_dict(self, args, account_currency=None, item=None):
+
+        if args["remarks"] != "Rounding gain/loss Entry for Stock Transfer":
+            if item:
+                stock_account = frappe.db.get_value('Item Default',{"parent":item.item_code, "company":self.company},'default_stock_account')
+                if stock_account:
+                    account_currency = frappe.db.get_value('Account',stock_account,'account_currency')
+                    if args["debit"] < 0:
+                        args["against"] = stock_account
+                    else: 
+                        args["account"] = stock_account
+        
+        return super().get_gl_dict(args, account_currency, item)
