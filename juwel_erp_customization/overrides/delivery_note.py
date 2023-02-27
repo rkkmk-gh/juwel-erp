63f3d46279ec0ba423e321dcae98bc9de4c20f99
rkkmk-gh <60239664+rkkmk-gh@users.noreply.github.com>
2023-02-27 10:56:29 -0500

    Update delivery_note.py

--- juwel_erp_customization/overrides/delivery_note.py
+++ juwel_erp_customization/overrides/delivery_note.py
@@ -1,8 +1,20 @@
-from erpnext.stock.doctype.delivery_note import DeliveryNote
+import frappe
+from frappe import _
+from erpnext.stock.doctype.delivery_note.delivery_note import DeliveryNote
+
 
 class JGDeliveryNote(DeliveryNote):
+
     def get_gl_dict(self, args, account_currency=None, item=None):
-        if item and item.stock_account:
-            args.account = item.stock_account
+
+        if args["remarks"] != "Rounding gain/loss Entry for Stock Transfer":
+            if item:
+                stock_account = frappe.db.get_value('Item Default',{"parent":item.item_code, "company":self.company},'default_stock_account')
+                if stock_account:
+                    account_currency = frappe.db.get_value('Account',stock_account,'account_currency')
+                    if args["debit"] < 0:
+                        args["against"] = stock_account
+                    else: 
+                        args["account"] = stock_account
         
-        super().get_gl_dict(args, account_currency, item)
+        return super().get_gl_dict(args, account_currency, item)
