80550b0e3c0552c3770e17665f0829387ed62c35
rkkmk-gh <60239664+rkkmk-gh@users.noreply.github.com>
2023-05-10 23:07:15 -0400

    Add files via upload

--- juwel_erp_customization/overrides/stock_reconciliation.py
+++ juwel_erp_customization/overrides/stock_reconciliation.py
@@ -1,9 +1,19 @@
 from erpnext.stock.doctype.stock_reconciliation.stock_reconciliation import StockReconciliation
+import frappe
+from frappe import _
 from .gl_utils import update_gl_dict
 
 class JGStockReconciliation(StockReconciliation):
 
     def get_gl_dict(self, args, account_currency=None, item=None):
         if item:
-            update_gl_dict(args, account_currency, item, self.company)        
+            item_code = frappe.db.get_value('Stock Reconciliation Item',{"name":item.name},'item_code')
+            stock_account = frappe.db.get_value('Item Default',{"parent":item_code, "company":self.company},'default_stock_account')
+
+            if stock_account:
+                if args.get('debit') > 0:
+                    args['account'] = stock_account
+                else:
+                    args['against'] = stock_account
+                    
             return super().get_gl_dict(args, account_currency, item)
