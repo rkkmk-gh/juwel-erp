afe334a7e83416fbb92b4c82a5e409e109e349ba
rkkmk-gh <60239664+rkkmk-gh@users.noreply.github.com>
2023-05-30 23:56:23 -0400

    Add files via upload

--- juwel_erp_customization/overrides/payment_entry.py
+++ juwel_erp_customization/overrides/payment_entry.py
@@ -9,6 +9,13 @@ class JGPaymentEntry(PaymentEntry):
 	#	super().calculate_taxes()
 	#	#tax is always in company currency
 	#	self.total_taxes_and_charges = self.base_total_taxes_and_charges
+	
+	def set_tax_withholding(self):
+		super().set_tax_withholding()
+		if self.apply_tax_withholding_amount:
+			for d in self.taxes:
+				if self.paid_from_account_currency != self.company_currency and d.charge_type == 'Actual':
+					d.tax_amount = d.tax_amount * self.source_exchange_rate
 
 	def set_amounts_after_tax(self):
 		applicable_tax = 0
@@ -56,11 +63,11 @@ class JGPaymentEntry(PaymentEntry):
 			self.base_paid_amount_after_tax = flt(
 				flt(self.paid_amount_after_tax) * flt(self.source_exchange_rate),
 					self.precision("base_paid_amount_after_tax"),
-				)
+			)
 				
 			self.base_received_amount_after_tax = flt(
 				flt(self.received_amount_after_tax) * flt(self.target_exchange_rate),
 				self.precision("base_paid_amount_after_tax"),
-				)
+			)
 		
 				
