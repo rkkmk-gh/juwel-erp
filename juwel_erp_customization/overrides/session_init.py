e2794842ed68ea34aeee038c935a9018acc0eb17
rkkmk-gh <60239664+rkkmk-gh@users.noreply.github.com>
2023-06-26 02:11:13 -0400

    Add files via upload

--- juwel_erp_customization/overrides/session_init.py
+++ juwel_erp_customization/overrides/session_init.py
@@ -4,8 +4,6 @@ from frappe import _
 def set_session_defaults(login_manager):
     try:
         base_account = frappe.db.get_value('Company',frappe.defaults.get_user_default('Company') ,'default_base_account')
-        print(frappe.defaults.get_user_default('Company'))
-        print(base_account)
         if base_account:
             frappe.defaults.set_user_default('base_account', base_account)
     except:
