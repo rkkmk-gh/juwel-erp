be2f7367d7af717f2e6a41451ef8fca1b13d5faa
rkkmk-gh <60239664+rkkmk-gh@users.noreply.github.com>
2025-02-04 22:34:36 -0500

    Update stock_entry.py

--- juwel_erp_customization/overrides/stock_entry.py
+++ juwel_erp_customization/overrides/stock_entry.py
@@ -7,15 +7,16 @@ class JGStockEntry(StockEntry):
 
     def get_gl_dict(self, args, account_currency=None, item=None):
         if item:
-            item_code, warehouse = frappe.db.get_value('Stock Entry Detail',{"name":item.name},['item_code', 's_warehouse'])  
-            stock_account = frappe.db.get_value('Item Default',{"parent":item_code, "company":self.company},'default_stock_account')
+            item_code, warehouse = frappe.db.get_value('Stock Entry Detail',{"name":item.name},['item_code', 's_warehouse'])
+            if warehouse:
+                stock_account = frappe.db.get_value('Item Default',{"parent":item_code, "company":self.company},'default_stock_account')
             
-            if stock_account:
-                warehouse_account = get_warehouse_account_map(self.company)[warehouse]["account"]
-                if args["account"] == warehouse_account:
-                    args["account"] = stock_account
-                    account_currency = frappe.db.get_value('Account',stock_account,'account_currency')
-                if args["against"] == warehouse_account: 
-                    args["against"] = stock_account
+                if stock_account:
+                    warehouse_account = get_warehouse_account_map(self.company)[warehouse]["account"]
+                    if args["account"] == warehouse_account:
+                        args["account"] = stock_account
+                        account_currency = frappe.db.get_value('Account',stock_account,'account_currency')
+                    if args["against"] == warehouse_account: 
+                        args["against"] = stock_account
         
         return super().get_gl_dict(args, account_currency, item)
